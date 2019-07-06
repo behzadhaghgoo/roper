326caf00f14e485ea4f9437302d15dc9a9d761a8
megsano <megsano@stanford.edu>
2019-07-05 21:40:35 -0700

    stuff

--- dqn.py
+++ dqn.py
@@ -13,12 +13,9 @@ Contains DQN and CNN-DQN classes
 class DQN(nn.Module):
     def __init__(self, num_inputs, num_actions):
         super(DQN, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(num_inputs, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-        )
-        self.fc = nn.Linear(128, num_actions)
+        self.fc1 = nn.Linear(num_inputs, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, num_actions)
         self.num_actions = num_actions
 
     def forward(self, x, return_latent = 'last'):
@@ -26,11 +23,22 @@ class DQN(nn.Module):
         	 return_latent: 'last': return last hidden vector
            								'state': return the state
         """
-        hidden = self.layers(x)
-        out = self.fc(F.relu(hidden))
-        if return_latent == "state":
-            return out, state
-        return out, hidden 
+        hidden1 = self.fc1(x)
+        hidden2 = self.fc2(F.relu(hidden1))
+        out = self.fc3(F.relu(hidden2))
+
+        if return_latent == 'state':
+        	return out, x 
+
+        elif return_latent == 'first_hidden':
+        	return out, hidden1 
+
+        elif return_latent == 'second_hidden':
+        	return out, hidden2 
+
+        else:
+        	print("Unrecognized return_latent argument")
+
 
     def act(self, state, epsilon):
         if random.random() > epsilon:
@@ -48,34 +56,34 @@ class Flatten(nn.Module):
         return x.view(N, -1)
 
 
-class CnnDQN(nn.Module):
-    def __init__(self, input_shape, num_actions):
-        super(CnnDQN, self).__init__()
+# class CnnDQN(nn.Module):
+#     def __init__(self, input_shape, num_actions):
+#         super(CnnDQN, self).__init__()
         
-        self.input_shape = input_shape
-        self.num_actions = num_actions
+#         self.input_shape = input_shape
+#         self.num_actions = num_actions
         
-        self.features = nn.Sequential(
-            nn.Conv2d(input_shape[-1], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-            #Flatten(),
-            #nn.Linear(self.feature_size(), 512),
-            #nn.ReLU(),
-        )
+#         self.features = nn.Sequential(
+#             nn.Conv2d(input_shape[-1], 32, kernel_size=8, stride=4),
+#             nn.ReLU(),
+#             nn.Conv2d(32, 64, kernel_size=4, stride=2),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 64, kernel_size=3, stride=1),
+#             nn.ReLU()
+#             #Flatten(),
+#             #nn.Linear(self.feature_size(), 512),
+#             #nn.ReLU(),
+#         )
         
-        self.fc = nn.Sequential(
-            nn.Linear(512, self.num_actions)
-        )
+#         self.fc = nn.Sequential(
+#             nn.Linear(512, self.num_actions)
+#         )
         
 
-    def forward(self, x, return_latent = 'last'):
-        print(x.shape)
-        hidden = self.features(x)
-        print(hidden.shape)
+#     def forward(self, x, return_latent = 'last'):
+#         print(x.shape)
+#         hidden = self.features(x)
+#         print(hidden.shape)
         out = self.fc(out)
         if return_latent == "state":
             return out, x
--- runexp.py
+++ runexp.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from tqdm import tqdm
 
 from .DQN import DQN, CnnDQN, update_target
-from .loss import TDLoss
+from .loss import TDLoss, StableTDLoss
 from .pbuffer import PrioritizedBuffer, AugmentedPrioritizedBuffer
 from .utils import *
 
@@ -21,181 +21,328 @@ epsilon_final = 0.01
 epsilon_decay = 500
 EPSILON_BY_FRAME = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
 
-def test(val_env, noisyGame, eps, num_val_trials, current_model):
-    rewards = []
-    for i in range(num_val_trials):
-        epsilon = 0
-        episode_reward = 0
-        state = val_env.reset()
-        state = np.append(state, float(noisyGame))
-        with torch.no_grad():
-            while True:
-                original_action = current_model.act(state, epsilon)
 
-                if original_action != int(original_action):
-                    original_action = original_action.numpy()[0]
+class MyTrainable(Trainable):
 
-                actual_action = original_action
-                next_state, reward, done, _ = val_env.step(actual_action)
-                next_state = np.append(next_state, float(noisyGame))
+	def _setup(self):
+		self.env, self.val_env = get_env(env_id)
 
-                # if noisyGame:
-                    # reward += random.uniform(-1., 1.)
+	def _train(self):
 
-                state = next_state
-                episode_reward += reward
+		hardcoded_reward = self.train_helper(True) 
+		non_hardcoded_reward = self.train_helper(False) 
 
-                if done:
-                    rewards.append(episode_reward)
-                    break
-    return np.mean(rewards)
+		return non_hardcoded_reward - hardcoded_reward
 
 
+	def train_helper(self, hardcoded):
 
-def train(env, val_env, 
-          method, var, mean,
-          decision_eps,
-          alpha, beta,
-          hardcoded, cnn,
-          invert_actions = False, num_frames = 30000, 
-          num_val_trials = 10, batch_size = 32, gamma = 0.99,
-          num_trials = 5, USE_CUDA = False, device = "", eps = 1., avg_stored=False):
+		config = self.config 
+
+		losses = []
+		all_rewards = []
+		standard_val_rewards = []
+		noisy_val_rewards = []
+		states_count_ratios = []
+		episode_reward = 0
+
+	    # Initialize state
+	    noisyGame = False
+	    state = self.env.reset()
+	    state = np.append(state, float(noisyGame)) 
+	    meta_state = (state, float(noisyGame))
+
+	    # Initialize replay buffer, model, TD loss, and optimizers
+	    # result_df = pd.DataFrame()
+	    power = config['theta']
+	    all_standard_val_rewards = []
+	    all_proportions = []
+	    std_weights = []
+	    noisy_weights = []
+	    std_buffer_example_count = []
+	    noisy_buffer_example_count = []
+
+		for t in range(num_trials):
+
+			self.training_iteration += 1 
+
+	        if cnn:
+	            current_model = CnnDQN(self.env.observation_space.shape, self.env.action_space.n)
+	            target_model  = CnnDQN(self.env.observation_space.shape, self.env.action_space.n)
+	        else:
+	            current_model = DQN(self.env.observation_space.shape[0] + 1, self.env.action_space.n) 
+	            target_model  = DQN(self.env.observation_space.shape[0] + 1, self.env.action_space.n)
+
+	        if config['method'] == 'ours':
+	        	td_loss = StableTDLoss()
+	        else:
+	        	td_loss = TDLoss()
+
+	        optimizer = optim.Adam(current_model.parameters(), lr=config['lr'])
+
+
+	        if USE_CUDA:
+	            current_model = current_model.cuda()
+	            target_model  = target_model.cuda()
+
+
+	        replay_buffer = PrioritizedBuffer(int(1e6))
+
+	        print("trial number: {}".format(t))
+
+	        for frame_idx in range(1, num_frames + 1):
+	            epsilon = EPSILON_BY_FRAME(frame_idx)
+	            original_action = current_model.act(state, epsilon)
+
+	            # If in noisy environment, make action random with probability eps
+	            if noisyGame and random.uniform(0,1) < config['decision_eps']:
+	                if config['invert_actions']:
+	                    actual_action = 1 - original_action # invert
+	                else:
+	                    actual_action = original_action
+	            else:
+	                actual_action = original_action
+
+	            next_state, reward, done, _ = self.env.step(actual_action)
+
+	            # If in noisy environment, make reward completely random
+	            if noisyGame:
+	                reward *= np.random.normal(config['mean'], config['var'])
+
+	            if not config['cnn']:
+	                next_state = np.append(next_state, float(noisyGame))
+	            meta_next_state = (next_state, float(noisyGame))
+	            
+	            # store q values and hidden states in buffer
+	            replay_buffer.push(meta_state, original_action, reward, meta_next_state, done)
+
+	            meta_state = meta_next_state
+	            episode_reward += reward
+
+	            if done:
+	                noisyGame = 1-noisyGame
+	                state = self.env.reset()
+	                state = np.append(state, float(noisyGame))
+	                meta_state = (state, float(noisyGame))
+	                all_rewards.append(episode_reward)
+	                episode_reward = 0
+
+	            if len(replay_buffer) > config['batch_size'] and frame_idx % 4 == 0:
+	                beta = BETA_BY_FRAME(frame_idx)
+	                loss = td_loss.compute_td_loss(current_model, target_model, beta, replay_buffer, optimizer)
+	                losses.append(loss.data.tolist())
+
+	            if frame_idx % 200 == 0:
+	                all_standard_val_rewards.append(self._test(False, config['eps'], config['num_val_trials'], current_model))
+	                all_proportions.append(float(replay_buffer.states_count[1]) / (float(replay_buffer.states_count[1])  + float(replay_buffer.states_count[0])))
+	                weight_dict = replay_buffer.get_average_weight_by_env()
+	                std_weights.append(weight_dictconfig['std_avg'])
+	                noisy_weights.append(weight_dictconfig['noisy_avg'])
+	                std_buffer_example_count.append(weight_dictconfig['std_count'])
+	                noisy_buffer_example_count.append(weight_dictconfig['noisy_count'])
+	                
+
+	            if frame_idx % 1000 == 0:
+	                print("Frame {}".format(frame_idx))
+	                update_target(current_model, target_model)
+
+		#return tune.TrainingResult(timesteps_this_iter=n, mean_loss=validation_loss)
+		return np.mean(all_standard_val_rewards)
+
+
+	def _test(noisyGame, eps, num_val_trials, current_model):
+
+	    rewards = []
+
+	    for i in range(num_val_trials):
+	        epsilon = 0
+	        episode_reward = 0
+	        state = self.val_env.reset()
+	        state = np.append(state, float(noisyGame))
+
+	        with torch.no_grad():
+	            while True:
+	                original_action = current_model.act(state, epsilon)
+
+	                if original_action != int(original_action):
+	                    original_action = original_action.numpy()[0]
+
+	                actual_action = original_action
+	                next_state, reward, done, _ = self.val_env.step(actual_action)
+	                next_state = np.append(next_state, float(noisyGame))
+
+	                state = next_state
+	                episode_reward += reward
+
+	                if done:
+	                    rewards.append(episode_reward)
+	                    break
+
+	    return np.mean(rewards)
+
+	def _stop(self):
+
+
+	def _save(self, checkpoint_dir):
+		path = checkpoint_dir + '/save'
+		return self.saver.save()
+
+
+
+# def train(self):
+
+# 	config = self.config 
+
+# 	'''
+# 	hyperparams 
+
+# 	method - 'average_over_batch', 'PER'
+# 	var
+# 	mean
+#     decision_eps,
+#     alpha, beta,
+#     hardcoded, cnn,
+#     invert_actions = False, 
+#     num_frames = 30000, 
+#     num_val_trials = 10, 
+#     batch_size = 32, 
+#     gamma = 0.99,
+#     num_trials = 5, 
+#     USE_CUDA = False, 
+#     device = "", 
+#     eps = 1., 
+#     avg_stored=False
+# 	'''
     
-    if USE_CUDA:
-        device = torch.device("cuda")
+#     if USE_CUDA:
+#         device = torch.device("cuda")
 
-    """Args:"""
-    losses = []
-    all_rewards = []
-    standard_val_rewards = []
-    noisy_val_rewards = []
-    states_count_ratios = []
-    episode_reward = 0
+#     """Args:"""
+#     losses = []
+#     all_rewards = []
+#     standard_val_rewards = []
+#     noisy_val_rewards = []
+#     states_count_ratios = []
+#     episode_reward = 0
 
-    # Initialize state
-    noisyGame = False
-    state = env.reset()
-    state = np.append(state, float(noisyGame)) # BACK IN
-    meta_state = (state, float(noisyGame))
+#     # Initialize state
+#     noisyGame = False
+#     state = config['env'].reset()
+#     state = np.append(state, float(noisyGame)) 
+#     meta_state = (state, float(noisyGame))
 
-    # Initialize replay buffer, model, TD loss, and optimizers
+#     # Initialize replay buffer, model, TD loss, and optimizers
 
-    result_df = pd.DataFrame()
-    theta = 1.
-    power = theta
-    all_standard_val_rewards = []
-    all_proportions = []
-    std_weights = []
-    noisy_weights = []
-    std_buffer_example_count = []
-    noisy_buffer_example_count = []
+#     result_df = pd.DataFrame()
+#     theta = 1.
+#     power = config['theta']
+#     all_standard_val_rewards = []
+#     all_proportions = []
+#     std_weights = []
+#     noisy_weights = []
+#     std_buffer_example_count = []
+#     noisy_buffer_example_count = []
 
-    for t in range(num_trials):
+#     for t in range(num_trials):
 
-        if cnn:
-            current_model = CnnDQN(env.observation_space.shape, env.action_space.n)
-            target_model  = CnnDQN(env.observation_space.shape, env.action_space.n)
-        else:
-            current_model = DQN(env.observation_space.shape[0] + 1, env.action_space.n) # BACK IN
-            target_model  = DQN(env.observation_space.shape[0] + 1, env.action_space.n) # BACK IN
-        td_loss = TDLoss(method=method)
-
-        optimizer = optim.Adam(current_model.parameters())
-    
-    # Multi GPU - Under Construction.
-#     current_model = current_model.to(device)
-#     target_model = target_model.to(device)
-
-#     # Single GPU Code
-        if USE_CUDA:
-            current_model = current_model.cuda()
-            target_model  = target_model.cuda()
-
-
+#         if cnn:
+#             current_model = CnnDQN(env.observation_space.shape, env.action_space.n)
+#             target_model  = CnnDQN(env.observation_space.shape, env.action_space.n)
+#         else:
+#             current_model = DQN(env.observation_space.shape[0] + 1, env.action_space.n) 
+#             target_model  = DQN(env.observation_space.shape[0] + 1, env.action_space.n)
         
-        if method=='average_over_buffer':
-            replay_buffer = AugmentedPrioritizedBuffer(int(1e6))
-        else:
-            replay_buffer = PrioritizedBuffer(int(1e6))
+#         td_loss = TDLoss(method=config['method'])
 
-        print("trial number: {}".format(t))
-        if method=='average_over_buffer':
-            replay_buffer = AugmentedPrioritizedBuffer(int(1e6))
-        else:
-            replay_buffer = PrioritizedBuffer(int(1e6))
-        for frame_idx in range(1, num_frames + 1):
-            epsilon = EPSILON_BY_FRAME(frame_idx)
-            original_action = current_model.act(state, epsilon)
+#         optimizer = optim.Adam(current_model.parameters())
 
-            # If in noisy environment, make action random with probability eps
-            if noisyGame and random.uniform(0,1) < decision_eps:
-                if invert_actions:
-                    actual_action = 1 - original_action # invert
-                else:
-                    actual_action = original_action
-            else:
-                actual_action = original_action
 
-            next_state, reward, done, _ = env.step(actual_action)
+# #     # Single GPU Code
+#         if USE_CUDA:
+#             current_model = current_model.cuda()
+#             target_model  = target_model.cuda()
 
-            # If in noisy environment, make reward completely random
-            if noisyGame:
-                reward *= np.random.normal(mean, var)
 
-            if not cnn:
-                next_state = np.append(next_state, float(noisyGame))
-            meta_next_state = (next_state, float(noisyGame))
+#         if config['method']=='average_over_buffer':
+#             replay_buffer = AugmentedPrioritizedBuffer(int(1e6))
+#         else:
+#             replay_buffer = PrioritizedBuffer(int(1e6))
+
+#         print("trial number: {}".format(t))
+
+#         for frame_idx in range(1, config['num_frames'] + 1):
+#             epsilon = EPSILON_BY_FRAME(frame_idx)
+#             original_action = current_model.act(state, epsilon)
+
+#             # If in noisy environment, make action random with probability eps
+#             if noisyGame and random.uniform(0,1) < config['decision_eps']:
+#                 if invert_actions:
+#                     actual_action = 1 - original_action # invert
+#                 else:
+#                     actual_action = original_action
+#             else:
+#                 actual_action = original_action
+
+#             next_state, reward, done, _ = config['env'].step(actual_action)
+
+#             # If in noisy environment, make reward completely random
+#             if noisyGame:
+#                 reward *= np.random.normal(config['mean'], var)
+
+#             if not cnn:
+#                 next_state = np.append(next_state, float(noisyGame))
+#             meta_next_state = (next_state, float(noisyGame))
             
-            # store q values and hidden states in buffer
-            if method=='average_over_buffer':
-                state_var = Variable(torch.FloatTensor(np.float32(state)))
-                with torch.no_grad():
-                    q_values, hiddens = current_model.forward(state_var, return_latent = "last")
-                replay_buffer.push(meta_state, original_action, reward, meta_next_state, done, hiddens, q_values)
-            else:
-                replay_buffer.push(meta_state, original_action, reward, meta_next_state, done)
+#             # store q values and hidden states in buffer
+#             if config['method']=='average_over_buffer':
+#                 state_var = Variable(torch.FloatTensor(np.float32(state)))
+#                 with torch.no_grad():
+#                     q_values, hiddens = current_model.forward(state_var, config['return_latent'] = "last")
+#                 replay_buffer.push(meta_state, original_action, reward, meta_next_state, done, hiddens, q_values)
+#             else:
+#                 replay_buffer.push(meta_state, original_action, reward, meta_next_state, done)
 
-            meta_state = meta_next_state
-            episode_reward += reward
+#             meta_state = meta_next_state
+#             episode_reward += reward
 
-            if done:
-                noisyGame = 1-noisyGame
-                state = env.reset()
-                state = np.append(state, float(noisyGame))
-                meta_state = (state, float(noisyGame))
-                all_rewards.append(episode_reward)
-                episode_reward = 0
+#             if done:
+#                 noisyGame = 1-noisyGame
+#                 state = env.reset()
+#                 state = np.append(state, float(noisyGame))
+#                 meta_state = (state, float(noisyGame))
+#                 all_rewards.append(episode_reward)
+#                 episode_reward = 0
 
-            if len(replay_buffer) > batch_size and frame_idx % 4 == 0:
-                beta = BETA_BY_FRAME(frame_idx)
-                loss = td_loss.compute_td_loss(current_model, target_model, beta, replay_buffer, optimizer)
-                losses.append(loss.data.tolist())
+#             if len(replay_buffer) > batch_size and frame_idx % 4 == 0:
+#                 beta = BETA_BY_FRAME(frame_idx)
+#                 loss = td_loss.compute_td_loss(current_model, target_model, beta, replay_buffer, optimizer)
+#                 losses.append(loss.data.tolist())
 
-            if frame_idx % 200 == 0:
-                all_standard_val_rewards.append(test(val_env, False, eps, num_val_trials, current_model))
-                all_proportions.append(float(replay_buffer.states_count[1]) / (float(replay_buffer.states_count[1])  + float(replay_buffer.states_count[0])))
-                weight_dict = replay_buffer.get_average_weight_by_env()
-                std_weights.append(weight_dict['std_avg'])
-                noisy_weights.append(weight_dict['noisy_avg'])
-                std_buffer_example_count.append(weight_dict['std_count'])
-                noisy_buffer_example_count.append(weight_dict['noisy_count'])
+#             if frame_idx % 200 == 0:
+#                 all_standard_val_rewards.append(test(val_env, False, eps, num_val_trials, current_model))
+#                 all_proportions.append(float(replay_buffer.states_count[1]) / (float(replay_buffer.states_count[1])  + float(replay_buffer.states_count[0])))
+#                 weight_dict = replay_buffer.get_average_weight_by_env()
+#                 std_weights.append(weight_dictconfig['std_avg'])
+#                 noisy_weights.append(weight_dictconfig['noisy_avg'])
+#                 std_buffer_example_count.append(weight_dictconfig['std_count'])
+#                 noisy_buffer_example_count.append(weight_dictconfig['noisy_count'])
                 
                 
-            #         plot(frame_idx, all_rewards, losses, standard_val_rewards, noisy_val_rewards, states_count_ratios)
+#             #         plot(frame_idx, all_rewards, losses, standard_val_rewards, noisy_val_rewards, states_count_ratios)
 
-            if frame_idx % 1000 == 0:
-                print("Frame {}".format(frame_idx))
-                update_target(current_model, target_model)
+#             if frame_idx % 1000 == 0:
+#                 print("Frame {}".format(frame_idx))
+#                 update_target(current_model, target_model)
 
-    print(len(all_proportions))
+#     print(len(all_proportions))
     
-    result_df['frame'] = 200*np.arange(len(all_proportions)) % num_frames
-    result_df['trial_num'] = np.floor(200 *np.arange(len(all_proportions)) / num_frames)
-    result_df['val_reward'] = all_standard_val_rewards
-    result_df['proportion'] = all_proportions
-    result_df['std_weights'] = std_weights
-    result_df['noisy_weights'] = noisy_weights
-    result_df['std_buffer_example_count'] = std_buffer_example_count
-    result_df['noisy_buffer_example_count'] = noisy_buffer_example_count
-    return result_df
+#     result_dfconfig['frame'] = 200*np.arange(len(all_proportions)) % num_frames
+#     result_dfconfig['trial_num'] = np.floor(200 *np.arange(len(all_proportions)) / num_frames)
+#     result_dfconfig['val_reward'] = all_standard_val_rewards
+#     result_dfconfig['proportion'] = all_proportions
+#     result_dfconfig['std_weights'] = std_weights
+#     result_dfconfig['noisy_weights'] = noisy_weights
+#     result_dfconfig['std_buffer_example_count'] = std_buffer_example_count
+#     result_dfconfig['noisy_buffer_example_count'] = noisy_buffer_example_count
+#     return result_df
 
